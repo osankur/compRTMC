b715ae2f533fcef8ca95f538b56567d462741410
Ocan Sankur <ocan.sankur@irisa.fr>
2022-04-13 18:50:02 +0200

    easier leader script

--- comprtmc/resources/examples/leader/generate.sh
+++ comprtmc/resources/examples/leader/generate.sh
@@ -1,9 +1,8 @@
-python3 leader.py -n 2 -i 0 --timedSMV > leader2a.tsmv
-python3 leader.py -n 2 -i 1 --timedSMV > leader2b.tsmv
-python3 leader.py -n 2 -i 0 > leader2a-fsm.smv 2> leader2a-ta.ta
-python3 leader.py -n 2 -i 1 > leader2b-fsm.smv 2> leader2b-ta.ta
-
-python3 leader.py -n 3 -i 0 --timedSMV > leader3a.tsmv
-python3 leader.py -n 3 -i 1 --timedSMV > leader3b.tsmv
-python3 leader.py -n 3 -i 0 > leader3a-fsm.smv 2> leader3a-ta.ta
-python3 leader.py -n 3 -i 1 > leader3b-fsm.smv 2> leader3b-ta.ta
+for n in `seq 2 2`
+do
+    for i in `seq 0 2`
+    do
+        python3 leader-easy.py -n $n -i $i --timedSMV > leader_n${n}_i${i}.tsmv
+        python3 leader-easy.py -n $n -i $i > leader_n${n}_i${i}-fsm.smv 2> leader_n${n}_i${i}-ta.ta
+    done
+done
---
+++ comprtmc/resources/examples/leader/leader-easy.py
@@ -0,0 +1,171 @@
+import argparse
+import sys
+#
+# Simplified version where only process 0 can crash
+# The timed language is too complex to learn even for simple timings
+# cmd is not updated correctly: see the error trace in leader2a.tsmv
+#
+# remove receive0, wakeup_timeout0 and implement the timeout with heartbeats
+# put back the crash{i} events
+
+
+#-- Delporte-Gallet, Devismes, Fauconnier. SSS'07. Robust Stabilizing Leader Election.
+computation = [
+        [(7,11), (2,4), (3,3)],
+        [(4,4), (4,4), (4,4)],
+        [(1,1), (1,1), (1,1)],
+    ]
+crash_time = [
+        (15,23),
+        (8,8),
+        (5,5)
+    ]   
+timeout_delay = 3
+max_cnt = 15
+max_crash = 3
+max_hb = 4
+def dump(n, computation, crash_time, timeout_delay, max_cnt, max_crash, timedSMV):
+    if timedSMV:
+        print("@TIME_DOMAIN continuous")
+    print("MODULE proc(id, wakeup, crash, broadcasting, recv_id)")
+    print("VAR")
+    print("\tstate : {ok, sending, crashed};")
+    print(F"\tleader : 0..{n};")
+    print(F"\thb : 0..{max_hb};")
+    print(F"""ASSIGN
+    init(leader) := id;
+    next(leader) := case
+        wakeup & hb = {max_hb}: id;
+        broadcasting & recv_id < id : recv_id;
+        TRUE : leader;
+    esac;
+    init(hb) := 0;
+    next(hb) := case
+        wakeup & hb < {max_hb} : hb + 1;
+        broadcasting & recv_id <= leader : 0;
+        TRUE : hb;
+    esac;
+    next(state) := case
+        state = ok & wakeup : sending;
+        state = sending : ok;
+        crash : crashed;
+        state = crashed & wakeup : ok;
+        TRUE : state;
+    esac;
+
+MODULE main
+FROZENVAR
+        """)
+    for i in range(n):
+        print(F"\tid{i} : 0..{n};")
+    print("VAR")
+    print("\tcmd : {cmd_broadcast", end="")
+    for i in range(n):
+        print(F", cmd_wakeup{i}, cmd_crash{i}", end="")
+    print("};")
+    print(F"""
+    cnt : 0..{max_cnt};
+    crash_cnt : 0..{max_crash};""")
+    #print("p0 : proc(id0, cmd = cmd_wakeup_ok0, cmd = cmd_wakeup_timeout0, cmd = cmd_crash, cmd = cmd_received0, msg);")    
+    #for i in range(1,n):
+    #    print(F"p{i} : proc(id{i}, cmd = cmd_wakeup_ok{i}, cmd = cmd_wakeup_timeout{i}, FALSE, cmd = cmd_received{i}, msg);")
+    for i in range(0,n):
+        print(F"p{i} : proc(id{i}, cmd = cmd_wakeup{i}, cmd = cmd_crash{i}, cmd = cmd_broadcast, msg);")
+    if timedSMV:
+        print(F"""
+     y : array 0..{n-1} of clock;
+            """)
+        print("INIT\n TRUE ")
+        for i in range(n):
+            print(F" & y[{i}] = 0")
+    print("ASSIGN")
+    if timedSMV:
+        for i in range(n):
+            print(F"""
+        next(y[{i}]) := case
+            cmd = cmd_wakeup{i} : 0;
+            TRUE : y[{i}];
+        esac;""")
+    crash = " | ".join([F"cmd = cmd_crash{i}" for i in range(n)])
+    print(F"""
+    init(cnt) := 0;
+    init(crash_cnt) := 0;
+    next(crash_cnt) := case
+        ({crash}) & crash_cnt < {max_crash} : crash_cnt + 1;
+        TRUE : crash_cnt;
+    esac;
+    next(cnt) := case
+        (cmd = cmd_wakeup0) & cnt < {max_cnt} : cnt + 1;
+        crash_cnt != next(crash_cnt) : 0;
+        TRUE : cnt;
+    esac;""")
+    print("INVAR\n\tTRUE ")
+    for i in range(n):
+        print(F"& id{i} = {i} ", end="")
+    # for i in range(n):
+    #     for j in range(i+1,n):
+    #         print(F"\t & (id{i} != id{j})",end="")
+    # print("\t& (" + " | ".join(map(lambda x:F"id{x} = 0", range(n))) + ")")
+    print("")
+    print("& (cmd = cmd_broadcast <-> (",end="")
+    print(" | ".join([F"p{i}.state = sending" for i in range(n)]) + "))")
+    if timedSMV:
+        for i in range(n):
+            print(F"& ((cmd = cmd_wakeup{i} & p{i}.state = ok) -> ({computation[i][0]} <= y[{i}] & y[{i}] <= {computation[i][1]})) -- wakeup period")
+            print(F"& (p{i}.state = ok -> y[{i}] <= {computation[i][1]}) -- invariant")
+            print(F"& (p{i}.state = crashed -> y[{i}] <= {crash_time[1]}) -- invariant")
+            print(F"& ((cmd = cmd_wakeup{i} & p{i}.state = crashed) -> ({crash_time[0]} <= y[{i}] & y[{i}] <= {crash_time[1]})) -- crash time")
+    print(F"& (({crash}) -> crash_cnt < {max_crash})")
+    print("""
+DEFINE 
+    msg := case""")
+    for i in range(n):
+        print(F"\tp{i}.state = sending : p{i}.leader;")
+    print(F"""\tTRUE : {n};\nesac;""")
+    print("\tstable := " + " & ".join([F"p{i}.leader = 0" for i in range(n)]) + ";")
+    print("\terr := cnt=15 & !stable;")
+    for i in range(n):
+        print(F"\t_rt_wakeup{i} := cmd = cmd_wakeup{i};")
+        print(F"    _rt_crash{i} := cmd = cmd_crash{i};")
+    print("INVARSPEC !err")
+    ta_fout = sys.stderr
+    if not timedSMV:
+        print(F"system:leader{n}_ta",file=ta_fout)
+        for i in range(n):
+            print(F"event:wakeup{i}",file=ta_fout)
+            print(F"event:crash{i}",file=ta_fout)
+        print("",file=ta_fout)
+        print(F"clock:{n}:y",file=ta_fout)
+        print("",file=ta_fout)
+        # i = 0
+        # print(F"process:P{i}", file=ta_fout)
+        # print(F"location:P{i}:ok" + "{initial::invariant:" + F"y[{i}] <= {computation[i][1]}" + "}",file=ta_fout)
+        # print("location:P" + str(i) + ":crashed{invariant:" + F"y[{i}] <= {crash_time[1]}" + "}",file=ta_fout)
+        # print(F"edge:P{i}:ok:ok:wakeup_ok{i}" + "{provided: " + F"y[{i}]>={computation[i][0]} && y[{i}] <= {computation[i][1]} && x[{i}] < {timeout_delay}: do : y[{i}] = 0" + "}",file=ta_fout)
+        # print(F"edge:P{i}:ok:ok:wakeup_timeout{i}" + "{provided: " + F"y[{i}]>={computation[i][0]} && y[{i}] <= {computation[i][1]} && x[{i}] >= {timeout_delay} : do : y[{i}] = 0" + "}", file=ta_fout)
+        # print(F"edge:P{i}:ok:ok:received{i}" + "{do : " + F"x[{i}] = 0" + "}",file=ta_fout)
+        # print(F"edge:P{i}:ok:crashed:crash" + "{" + F"do : y[{i}] = 0" + "}",file=ta_fout)
+        # print(F"edge:P{i}:crashed:ok:wakeup_ok{i}" + "{provided: " + F"y[{i}] >= {crash_time[0]} && y[{i}] <= {crash_time[1]} : do : y[{i}] = 0" + "}",file=ta_fout)
+        for i in range(1,n):
+            print(F"process:P{i}", file=ta_fout)
+            print(F"location:P{i}:ok" + "{initial::invariant:" + F"y[{i}] <= {computation[i][1]}" + "}",file=ta_fout)
+            print(F"location:P{i}:crashed" + "{invariant:" + F"y[{i}] <= {crash_time[1]}" + "}",file=ta_fout)
+            print(F"edge:P{i}:ok:ok:wakeup{i}" + "{provided: " + F"y[{i}]>={computation[i][0]} && y[{i}] <= {computation[i][1]} : do : y[{i}] = 0" + "}",file=ta_fout)
+            print(F"edge:P{i}:ok:crashed:crash{i}" + "{" + F"do : y[{i}] = 0" + "}",file=ta_fout)
+            print(F"edge:P{i}:crashed:ok:wakeup{i}" + "{provided: " + F"y[{i}] >= {crash_time[0]} && y[{i}] <= {crash_time[1]} : do : y[{i}] = 0" + "}",file=ta_fout)
+
+def main():
+    parser = argparse.ArgumentParser(description="Real-Time bit-counter broadcast protocol generator")
+    parser.add_argument("-n", type=int,
+                        help="number of bits / processes", required=True)
+    parser.add_argument("-t", "--timedSMV", action='store_true',
+                        help=("Generate SMV timed automaton."))
+    parser.add_argument("-i", "--index", type=int, required=True, help="index of the delays")
+    args = parser.parse_args()
+    n = args.n
+    timedSMV = args.timedSMV
+    i = args.index
+    assert(i >= 0 and i <= len(computation)-1)
+    dump(n, computation[i], crash_time[i], timeout_delay, max_cnt, max_crash, timedSMV)
+if __name__ == "__main__":
+    main()
